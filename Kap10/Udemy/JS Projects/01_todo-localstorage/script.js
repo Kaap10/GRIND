748040547929101edd7a06886dce263679e0075f
Vardhman Gupta <vardhmangupta2004@gmail.com>
2024-12-09 19:51:39 +0530

    updated git

--- Kap10/Udemy/JS Projects/01_todo-localstorage/script.js
+++ Kap10/Udemy/JS Projects/01_todo-localstorage/script.js
@@ -1,4 +1,28 @@
-const todoInput = document.getElementById("todo-input")
-const addTaskButton = document.getElementById("add-task-btn")
-const todoList = document.getElementById("todo-list")
+document.addEventListener("DOMContentLoaded", () => {
+    const todoInput = document.getElementById("todo-input");
+    const addTaskButton = document.getElementById("add-task-btn");
+    const todoList = document.getElementById("todo-list");
+
+    const tasks = [];
+
+    addTaskButton.addEventListener("click", () => {
+        const taskText = todoInput.value.trim();
+        if(taskText === "") return;
+
+        const newTask = {
+            id: Date.now(),
+            text: taskText,
+            completed:false,
+        };
+
+        tasks.push(newTask);
+        console.log(tasks);
+
+        const li = document.createElement("li");
+        li.textContent = taskText;
+
+        todoList.appendChild(li);
+        todoInput.value = "";
+    })
+})
 
